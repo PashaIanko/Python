898e62d7ee6c20f6a68a6f2a123aebd735c0affd
Pavel <yanpavevg@yandex.ru>
2020-02-04 19:01:07 +0700

    setheading

--- Task 2 Turtles/Main.py
+++ Task 2 Turtles/Main.py
@@ -4,8 +4,9 @@ from random import seed
 from random import random
 from random import randint
 
-turtle_speed = 3;
-turtles_numb = 10;
+turtle_speed = 50;
+turtles_numb = 3;
+dt = 1;
 
 screen_width = 500;
 screen_height = 500;
@@ -19,19 +20,14 @@ turtles = [];
 for _ in range(turtles_numb):
     turtles.append(turtle.Turtle());
 
-offset = 10;
 y_from = -screen_height/2;
 y_to = -y_from;
 x_from = -screen_width/2;
 x_to = -x_from;
 
-
-
-
 for i in range(turtles_numb):
     x_rand = rand.randrange(x_from, x_to);
     y_rand = rand.randrange(y_from, y_to);
-    print(x_rand, y_rand);
     turtles[i].color((randint(0,255), randint(0,255), randint(0,255)));
     turtles[i].penup();
     turtles[i].setpos(x_rand, y_rand);
@@ -39,3 +35,12 @@ for i in range(turtles_numb):
 
 #turning the first turtle at an arbitrary angle
 turtles[0].right(randint(0, 180));
+
+t = 0;
+turtles[0].forward(turtle_speed * dt);
+
+
+angle = turtles[1].towards(turtles[0]);
+turtles[1].setheading(angle);
+
+
