b82b80acd44f9574ee0842aff0fd6120c577e380
Pavel <yanpavevg@yandex.ru>
2020-02-04 18:15:16 +0700

    different colors

--- Task 2 Turtles/Main.py
+++ Task 2 Turtles/Main.py
@@ -2,6 +2,7 @@ import turtle
 import random as rand
 from random import seed
 from random import random
+from random import randint
 
 turtle_speed = 3;
 turtles_numb = 10;
@@ -10,6 +11,7 @@ screen_width = 500;
 screen_height = 500;
 screen = turtle.Screen();
 screen.setup(screen_width, screen_height);
+screen.colormode(255);
 
 
 
@@ -23,10 +25,17 @@ y_to = -y_from;
 x_from = -screen_width/2;
 x_to = -x_from;
 
+
+
+
 for i in range(turtles_numb):
     x_rand = rand.randrange(x_from, x_to);
     y_rand = rand.randrange(y_from, y_to);
     print(x_rand, y_rand);
+    turtles[i].color((randint(0,255), randint(0,255), randint(0,255)));
     turtles[i].penup();
     turtles[i].setpos(x_rand, y_rand);
+    turtles[i].pendown();
 
+#turning the first turtle at an arbitrary angle
+turtles[0].right(randint(0, 180));
