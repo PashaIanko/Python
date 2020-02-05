43bb039206528d7c3beca2312b79e9d2183048cc
Pavel <yanpavevg@yandex.ru>
2020-02-05 23:25:15 +0700

    Rocket explodes, particles stick to the ground

--- Task 3 Rocket/Rocket.py
+++ Task 3 Rocket/Rocket.py
@@ -5,6 +5,9 @@ from tkinter import *
 win_height = 600
 win_width = 600
 gravity = 0.2
+rocket_velocity = -8
+particles_numb = 50
+particles_velocity = 2
 dt = 1
 
 master = Tk()
@@ -26,18 +29,27 @@ class Particle:
         self.x = x
         self.y = y
         self.z = z
-        self.vx = random.random() * 2 - 1
-        self.vy = random.random() * 2 - 1
-        self.vz = random.random() * 2 - 1
+        self.V = particles_velocity
+        velocity_angle = random.uniform(0, 2*3.14)
+       
+        self.vx = self.V * math.cos(velocity_angle)
+        self.vy = self.V * math.sin(velocity_angle)
+        self.vz = 0
         self.color = randomColor()
+        self.unupdatable = False
 
     def update(self):
-        self.x += self.vx * dt
-        self.y += self.vy * dt
-        self.z += self.vz * dt
+        if self.unupdatable == False:
+            self.x += self.vx * dt
+            self.y += self.vy * dt
+            self.z += self.vz * dt
+            self.vy += gravity * dt
+            
+            if self.y >= win_height:
+                self.y = win_height
+                self.unupdatable = True
 
-        self.vy += gravity * dt
-        
+                  
 
     def draw(self):
         size = 2
@@ -45,30 +57,32 @@ class Particle:
                              fill=self.color)
         
 
-class Firework:
+class Rocket:
     def __init__(self, x, y, z):
         self.x = x
         self.y = y
         self.z = z
         self.vx = 0
-        self.vy = -10
+        self.vy = rocket_velocity
         self.vz = 0
         self.explodeTime = 10
-        self.remove = False
+        self.ready_explode = False
+        
 
     def update(self):
         self.x += self.vx * dt
         self.y += self.vy * dt
         self.z += self.vz * dt
 
+        
+
         self.vy += gravity * dt
         self.explodeTime -= 1/60
 
-        #if self.explodeTime <= 0 :
-        #    self.remove = True
+               
 
         if self.vy >= 0:
-            self.remove = True
+            self.ready_explode = True
 
             
             
@@ -77,25 +91,25 @@ class Firework:
                              fill="black")
         
 particles = []
-fireworks = []
+Rockets = []
 
-def spawnFirework(event):
-    fireworks.append(Firework(event.x, event.y, 0))
+def spawnRocket(event):
+    Rockets.append(Rocket(win_width/2, win_height, 0))
 
-win.bind("<Button-1>", spawnFirework)
+win.bind("<Button-1>", spawnRocket)
 
 def engine():
     win.delete("all")
 
-    for i in range(0, len(fireworks)):
-        if i>=len(fireworks) - 1:
+    for i in range(0, len(Rockets)):
+        if i>=len(Rockets) - 1:
             break
         
-        f = fireworks[i]
-        if f.remove == True:
-            for j in range (0, 100):
+        f = Rockets[i]
+        if f.ready_explode == True:
+            for j in range (0, particles_numb):
                 particles.append(Particle(f.x, f.y, f.z))
-            fireworks.pop(i)   
+            Rockets.pop(i)   
         
         f.update()
         f.draw()
@@ -111,6 +125,109 @@ def engine():
     master.after(20, engine)
 
 engine()
+
+"""from tkinter import Tk, Canvas, Frame, BOTH
+
+win_height = 600
+win_width = 600
+ground_thickness = 10
+rockets_numb = 1
+gravity = 0.2
+dt = 0.1
+
+class Rocket:
+    #canvas_to_draw = Canvas()
+    height = 20
+    width = 8
+    def __init__(self, x, y, z, canvas):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.vx = 0
+        self.vy = -10
+        self.vz = 0
+        self.explodeTime = 10
+        self.remove = False
+        self.canvas_to_draw = canvas
+        self.draw()
+
+    def update(self):
+        print("update")
+        self.x += self.vx * dt
+        self.y += self.vy * dt
+        self.z += self.vz * dt
+        print(self.x, self.y, self.z)
+        self.vy += gravity * dt
+        self.explodeTime -= 1/60
+
+        #self.canvas_to_draw.create_rectangle(self.x, self.y, self.x+self.width, self.y-self.height,
+        #                     fill="red")
+        #if self.vy >= 0:
+        #    self.remove = True
+
+    def draw(self):
+        self.canvas_to_draw.create_rectangle(self.x-2, self.y-4, self.x+2, self.y+5,
+                             fill="red")       
+            
+    #def draw(self, x, y):
+    #    self.canvas_to_draw.create_rectangle(x, y, x+self.width, y-self.height,
+    #                         fill="red")
+
+class Graphics(Frame):
+
+    rockets = []
+    rockets_numb
     
+    def __init__(self, parent, rockets_numb):
+        Frame.__init__(self, parent)
+        self.parent = parent
+        self.initUI()
+        self.rockets_numb = rockets_numb
         
+
+    def set_rockets(self, rockets_numb):
+        for i in range(0, rockets_numb):
+            self.rockets.append(Rocket(10, 0, 0, self.canvas))
+            print("drawing rocket")
+            #self.rockets[i].draw(10, win_height-ground_thickness)
+
+    def initUI(self):
+        self.parent.title("Rocket")
+        self.pack(fill = BOTH, expand = 1)
+
+        self.canvas = Canvas(self, width=win_width, height=win_height)
+        #ground
+        self.canvas.create_rectangle(0, win_height, win_width, win_height-ground_thickness, fill = "black")
+        self.canvas.pack(fill = BOTH, expand = 1)
+
+        self.set_rockets(rockets_numb)
+
+    
+
+
+    def engine(self):
+        print("engine", "len=", len(self.rockets))
+        self.canvas.delete("all")
+
+        for i in range(0, len(self.rockets)):
+            print(i)
+            r = self.rockets[i]
+            #if f.remove == True:
+            #    for j in range (0, 100):
+            #        particles.append(Particle(f.x, f.y, f.z))
+            #    fireworks.pop(i)   
         
+            r.update()
+            r.draw()
+        self.parent.after(20, self.engine)
+        
+def main():
+        
+    root = Tk()
+    GUI = Graphics(root, rockets_numb)
+    GUI.bind("<Button-1>", GUI.engine())
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    main()"""
