ea55e4251c359f4d4ca67002f48f03f7afbf48e0
Pavel <yanpavevg@yandex.ru>
2020-02-07 21:53:56 +0700

    added delta, meander

--- Discrete Fourier Transform/main.py
+++ Discrete Fourier Transform/main.py
@@ -3,43 +3,13 @@ from math import pi as PI
 import math
 import cmath
 import numpy as np
+from scipy import signal
+
+#my realization of dft
+import dft as my_dft
 
 
-def dft (data):
-    data_size = len(data)
-    result = []
-    if data_size:
-        
-        pi_div_N_mult_two = 2*PI/data_size #optimization
-        for k in range (0, data_size):
-            x_k_real = 0; #result
-            x_k_im = 0; #result
-            
-            for n in range (0, data_size):
-                x_n = data[n]
-                arg = k*n*pi_div_N_mult_two
-                x_k_real += x_n * math.cos(arg)
-                x_k_im -= x_n * math.sin(arg)
-            
-            result.append(complex(x_k_real, x_k_im))
-    
-    return result
-    
    
-        
-
-def square(data):
-    result_data = []
-    data_size = len(data)
-
-    
-    for i in range (0, data_size):
-        val = data[i]*data[i]
-        result_data.append(val)
-    return result_data
-
-
-
 def extract_real(complex_data):
     data_size = len(complex_data)
     result = []
@@ -76,7 +46,7 @@ def calc_and_plot(x, y, title):
     fft_lib_res_im = extract_im(fft_lib_res)
 
     #my_calc
-    my_res = dft(y)
+    my_res = my_dft.dft(y)
     my_res_real = extract_real(my_res)
     my_res_im = extract_im(my_res)
 
@@ -85,40 +55,55 @@ def calc_and_plot(x, y, title):
     diff_im = get_diff(fft_lib_res_im, my_res_im)
 
     fig = plt.figure()
-    subplot_sin = fig.add_subplot(1, 2, 1)
-    subplot_sin.set_title(title + " Fourier Image(Re)")
-    subplot_sin.xaxis.set_ticks_position('bottom')
-    subplot_sin.yaxis.set_ticks_position('left')
+    subplot = fig.add_subplot(1, 2, 1)
+    subplot.set_title(title + " Fourier Image(Re)")
+    subplot.xaxis.set_ticks_position('bottom')
+    subplot.yaxis.set_ticks_position('left')
     
-    subplot_sin.plot(x, my_res_real, 'b', label = 'My dft, Re')
-    subplot_sin.plot(x, fft_lib_res_real, 'r--', label = 'Lib dft, Re')
-    subplot_sin.plot(x, diff_re, 'g', label = 'difference')
-    subplot_sin.legend()
+    subplot.plot(x, my_res_real, 'b', label = 'My dft, Re')
+    subplot.plot(x, fft_lib_res_real, 'r--', label = 'Lib dft, Re')
+    subplot.plot(x, diff_re, 'g', label = 'difference')
+    subplot.plot(x, y, 'yellow', label = 'Original')
+    subplot.legend()
 
-    subplot_sin = fig.add_subplot(1, 2, 2)
-    subplot_sin.set_title(title + " Fourier Image(Im)")
-    subplot_sin.xaxis.set_ticks_position('bottom')
-    subplot_sin.yaxis.set_ticks_position('left')
+    subplot = fig.add_subplot(1, 2, 2)
+    subplot.set_title(title + " Fourier Image(Im)")
+    subplot.xaxis.set_ticks_position('bottom')
+    subplot.yaxis.set_ticks_position('left')
     
-    subplot_sin.plot(x, my_res_im, 'b', label = 'My dft, Im')
-    subplot_sin.plot(x, fft_lib_res_im, 'r--', label = 'Lib dft, Im')
-    subplot_sin.plot(x, diff_im, 'g', label = 'difference')
-    subplot_sin.legend()
+    subplot.plot(x, my_res_im, 'b', label = 'My dft, Im')
+    subplot.plot(x, fft_lib_res_im, 'r--', label = 'Lib dft, Im')
+    subplot.plot(x, diff_im, 'g', label = 'difference')
+    subplot.legend()
 
     fig.show()
     
 
 
-x = np.linspace(-np.pi, np.pi, 100)
-y=np.sin(x)
+x = np.linspace(-10 * np.pi, 10 * np.pi, 200)
+y = np.sin(x)
 calc_and_plot(x, y, 'sin(x)')
 
-
-x = np.linspace(-np.pi, np.pi, 100)
-y=np.sin(2*x)
-calc_and_plot(x, y, 'sin(2x)')
-
 x = np.linspace(-2*np.pi, 2*np.pi, 100)
-y=np.sin(10*x)
-calc_and_plot(x, y, 'sin(10x)')
+y = np.sin(10*x) + np.sin(5*x)
+calc_and_plot(x, y, 'sin(10x)+sin(5x)')
+
+#meander
+meander_frequency = 5
+x = np.linspace(0, 1, 1000, endpoint = True)
+y = signal.square(2 * np.pi * meander_frequency * x)
+calc_and_plot(x, y, 'meander, f='+str(meander_frequency))
+
+#exp(-x^2/2)
+x = np.linspace(-50, 50, 200)
+y = np.exp(-0.5 * np.square(x))
+calc_and_plot(x, y, 'exp(-x^2/2)')
+
         
+#Diracks 
+x = np.linspace(-100, 100, 200)
+y = []
+for i in range (0, len(x)):
+    y.append(0)
+y[round(len(y)/2)] = 100
+calc_and_plot(x, y, 'Dirac delta')
