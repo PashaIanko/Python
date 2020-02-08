8a84043fc62bdb14c8afae0fd71984f158c63212
Pavel <yanpavevg@yandex.ru>
2020-02-08 23:11:16 +0700

    all examples work

--- Task 4 Electric field visualizing/Wires.py
+++ Task 4 Electric field visualizing/Wires.py
@@ -29,6 +29,22 @@ def PHI(wire, x, y):
     r = np.hypot(x-r0[0], y-r0[1])
     return wire.charge_density / r
 
+
+def calc(wires, X, Y, ny, nx):
+    #initializing
+    Ex, Ey = np.zeros((ny, nx)), np.zeros((ny, nx))
+    phi = np.zeros((ny, nx))
+    Ex_ort, Ey_ort = np.zeros((ny, nx)), np.zeros((ny, nx))
+    for wire in wires:
+        ex, ey = E(wire, x=X, y=Y)
+        phi = PHI(wire, x = X, y = Y)
+        Ex += ex
+        Ey += ey
+    #create equipotential curves - orthogonal to the field lines
+    Ex_ort, Ey_ort = orthogonal_vect(Ex, Ey)
+    return Ex, Ey, Ex_ort, Ey_ort, phi
+
+
 def plot(x, y, Ex, Ey, Ex_ort, Ey_ort):
     #plot
     fig = plt.figure()
@@ -62,6 +78,9 @@ x = np.linspace(-2, 2, nx)
 y = np.linspace(-2, 2, ny)
 X, Y = np.meshgrid(x, y)
 
+
+#############################
+#two wires of the same charge
 wires_numb = 2
 wires = []
 left_charge_density = 2
@@ -69,21 +88,21 @@ right_charge_density = 2
 wires.append(Wire(-1, 0, left_charge_density))
 wires.append(Wire(1, 0, right_charge_density))
 
+#calc phi and field
+[Ex, Ey, Ex_ort, Ey_ort, phi] = calc(wires, X, Y, ny, nx)
+
+#plotting two identical wires
+plot(x, y, Ex, Ey, Ex_ort, Ey_ort)
 
 
-def calc(wires, X, Y, ny, nx):
-    #initializing
-    Ex, Ey = np.zeros((ny, nx)), np.zeros((ny, nx))
-    phi = np.zeros((ny, nx))
-    Ex_ort, Ey_ort = np.zeros((ny, nx)), np.zeros((ny, nx))
-    for wire in wires:
-        ex, ey = E(wire, x=X, y=Y)
-        phi = PHI(wire, x = X, y = Y)
-        Ex += ex
-        Ey += ey
-    #create equipotential curves - orthogonal to the field lines
-    Ex_ort, Ey_ort = orthogonal_vect(Ex, Ey)
-    return Ex, Ey, Ex_ort, Ey_ort, phi
+###################################
+#dipole
+#two wires
+wires.clear()
+left_charge_density = 3
+right_charge_density = -3
+wires.append(Wire(-0.3, 0, left_charge_density))
+wires.append(Wire(0.3, 0, right_charge_density))
 
 #calc phi and field
 [Ex, Ey, Ex_ort, Ey_ort, phi] = calc(wires, X, Y, ny, nx)
@@ -91,9 +110,10 @@ def calc(wires, X, Y, ny, nx):
 #plotting two identical wires
 plot(x, y, Ex, Ey, Ex_ort, Ey_ort)
 
- 
 
-######################
+
+ 
+############################
 #evenly charged strip
 wires.clear()
 strip_charge_density = 2
@@ -111,5 +131,63 @@ for i in range(0, wires_numb):
 
 #plotting two identical wires
 plot(x, y, Ex, Ey, Ex_ort, Ey_ort)
+
+
+
+
+
+##############################
+#evenly charged strip and a parallel wire
+wires.clear()
+
+wires.clear()
+strip_charge_density = 2
+wire_charge_density = 2
+strip_x_from = -1
+strip_x_to = 1
+wires_numb = 15 #How many wires (to model a strip) will be placed from x_from to x_to
+wire_to_strip_dist = 1
+
+wires_coords = np.linspace(strip_x_from, strip_x_to, wires_numb)
+
+for i in range(0, wires_numb):
+    wires.append(Wire(wires_coords[i], 0, wire_charge_density))
+
+#adding a single wire
+wires.append(Wire(0, wire_to_strip_dist, wire_charge_density))
+
+#calc phi and field
+[Ex, Ey, Ex_ort, Ey_ort, phi] = calc(wires, X, Y, ny, nx)
+
+#plotting two identical wires
+plot(x, y, Ex, Ey, Ex_ort, Ey_ort)
+
+
+
+#################################
+#two strips
+
+wires.clear()
+
+wires.clear()
+strip_charge_density = 2
+strip_x_from = -1
+strip_x_to = 1
+wires_numb = 15 #How many wires (to model a strip) will be placed from x_from to x_to
+dist_between_strips = 0.4
+
+wires_coords = np.linspace(strip_x_from, strip_x_to, wires_numb)
+
+for i in range(0, wires_numb):
+    wires.append(Wire(wires_coords[i], -dist_between_strips/2, wire_charge_density))
+    wires.append(Wire(wires_coords[i], dist_between_strips/2, wire_charge_density))
+
+
+#calc phi and field
+[Ex, Ey, Ex_ort, Ey_ort, phi] = calc(wires, X, Y, ny, nx)
+
+#plotting two identical wires
+plot(x, y, Ex, Ey, Ex_ort, Ey_ort)
+
         
         
