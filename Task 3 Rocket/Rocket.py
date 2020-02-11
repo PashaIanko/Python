dc72779d9b51baeacf3f2b830c7248d963bf95c5
Pavel <yanpavevg@yandex.ru>
2020-02-11 19:43:52 +0700

    added distribution

--- Task 3 Rocket/Rocket.py
+++ Task 3 Rocket/Rocket.py
@@ -1,6 +1,8 @@
 import math
 import random
 from tkinter import *
+import matplotlib.pyplot as plt
+from matplotlib.patches import Circle
 
 win_height = 600
 win_width = 800
@@ -9,8 +11,8 @@ gravity = 0.5
 rocket_velocity = -20
 rockets_numb = 5
 
-particles_numb = 20
-circle_particles_numb = 20
+particles_numb = 30
+circle_particles_numb = 30
 
 particles_size = 5
 circle_particles_size = 4
@@ -22,6 +24,7 @@ master = Tk()
 
 win = Canvas(master, width=win_width, height=win_height)
 win.pack()
+
 
 
 def rgb2hex (r, g, b):
@@ -39,11 +42,12 @@ class Particle:
         self.y = y
         self.z = z
         self.V = particles_velocity
-        velocity_angle = random.uniform(0, 2*3.14)
+        velocity_angle_phi = random.uniform(0, 2*3.14)
+        velocity_angle_theta = random.uniform(0, 3.14)
        
-        self.vx = self.V * math.cos(velocity_angle)
-        self.vy = self.V * math.sin(velocity_angle)
-        self.vz = 0
+        self.vx = self.V * math.cos(velocity_angle_phi)*math.cos(velocity_angle_theta)
+        self.vy = self.V * math.cos(velocity_angle_theta)#self.V * math.sin(velocity_angle_phi)*math.sin(velocity_angle_theta)
+        self.vz = self.V * math.sin(velocity_angle_phi)*math.sin(velocity_angle_theta)#self.V * math.cos(velocity_angle_theta)
         self.color = rgb2hex(100, 100, 100)
         self.unupdatable = False
 
@@ -109,12 +113,53 @@ def spawnRocket(event):
 def spawnRandomRockets(event):
     for i in range (0, rockets_numb):
         x_rand = random.randrange(100, win_width-15)
+        z_rand = random.randrange(100, win_width-15)
        # print("x_rand=", x_rand)
         y = win_height
-        Rockets.append(Rocket(x_rand, y, 0, randomColor()))
+        Rockets.append(Rocket(x_rand, y, z_rand, randomColor()))
 
 win.bind("<Button-1>", spawnRocket)
 win.bind("<Button-3>", spawnRandomRockets)
+
+def plot_distribution(particles, circle_particles):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    X_dat = []
+    C_X_dat = []
+    C_Y_dat = []
+    Y_dat = []
+    for particle in particles:
+        X_dat.append(particle.x)
+        Y_dat.append(particle.z)
+        
+    for c_particle in circle_particles:
+        C_X_dat.append(c_particle.x)
+        C_Y_dat.append(c_particle.z)
+
+    X_to = max(max(X_dat), max(C_X_dat))
+    X_from = min(min(X_dat), min(C_X_dat))
+
+    Y_to = max(max(Y_dat), max(C_Y_dat))
+    Y_from = min(min(Y_dat), min(C_Y_dat))
+    
+    ax.scatter(X_dat, Y_dat, color = 'gray', marker="s")
+    ax.scatter(C_X_dat, C_Y_dat, color = 'yellow', marker='o')
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_xlim(X_from, X_to)
+    ax.set_ylim(Y_from, Y_to)
+    fig.show()
+
+
+def all_landed(particles, circle_particles):
+    for particle in particles:
+        if particle.y != win_height :
+            return False
+    for c_particle in circle_particles:
+        if c_particle.y != win_height:
+            return False
+    return True
+
 
 
 def engine():
@@ -145,113 +190,17 @@ def engine():
         p = circle_particles[i]
         p.update()
         p.draw_circle()
+
+    
+    if(all_landed(particles, circle_particles) and len(particles)):#len(particles) and len(circle_particles) and flag_to_plot == False):
+        print("plotting distribution")
+        plot_distribution(particles, circle_particles)
+        master.mainloop()
+     
     
     master.after(20, engine)
     
 engine()
 
-"""from tkinter import Tk, Canvas, Frame, BOTH
-
-win_height = 600
-win_width = 600
-ground_thickness = 10
-rockets_numb = 1
-gravity = 0.2
-dt = 0.1
-
-class Rocket:
-    #canvas_to_draw = Canvas()
-    height = 20
-    width = 8
-    def __init__(self, x, y, z, canvas):
-        self.x = x
-        self.y = y
-        self.z = z
-        self.vx = 0
-        self.vy = -10
-        self.vz = 0
-        self.explodeTime = 10
-        self.remove = False
-        self.canvas_to_draw = canvas
-        self.draw()
-
-    def update(self):
-        print("update")
-        self.x += self.vx * dt
-        self.y += self.vy * dt
-        self.z += self.vz * dt
-        print(self.x, self.y, self.z)
-        self.vy += gravity * dt
-        self.explodeTime -= 1/60
-
-        #self.canvas_to_draw.create_rectangle(self.x, self.y, self.x+self.width, self.y-self.height,
-        #                     fill="red")
-        #if self.vy >= 0:
-        #    self.remove = True
-
-    def draw(self):
-        self.canvas_to_draw.create_rectangle(self.x-2, self.y-4, self.x+2, self.y+5,
-                             fill="red")       
-            
-    #def draw(self, x, y):
-    #    self.canvas_to_draw.create_rectangle(x, y, x+self.width, y-self.height,
-    #                         fill="red")
-
-class Graphics(Frame):
-
-    rockets = []
-    rockets_numb
-    
-    def __init__(self, parent, rockets_numb):
-        Frame.__init__(self, parent)
-        self.parent = parent
-        self.initUI()
-        self.rockets_numb = rockets_numb
-        
-
-    def set_rockets(self, rockets_numb):
-        for i in range(0, rockets_numb):
-            self.rockets.append(Rocket(10, 0, 0, self.canvas))
-            print("drawing rocket")
-            #self.rockets[i].draw(10, win_height-ground_thickness)
-
-    def initUI(self):
-        self.parent.title("Rocket")
-        self.pack(fill = BOTH, expand = 1)
-
-        self.canvas = Canvas(self, width=win_width, height=win_height)
-        #ground
-        self.canvas.create_rectangle(0, win_height, win_width, win_height-ground_thickness, fill = "black")
-        self.canvas.pack(fill = BOTH, expand = 1)
-
-        self.set_rockets(rockets_numb)
-
-    
 
 
-    def engine(self):
-        print("engine", "len=", len(self.rockets))
-        self.canvas.delete("all")
-
-        for i in range(0, len(self.rockets)):
-            print(i)
-            r = self.rockets[i]
-            #if f.remove == True:
-            #    for j in range (0, 100):
-            #        particles.append(Particle(f.x, f.y, f.z))
-            #    fireworks.pop(i)   
-        
-            r.update()
-            r.draw()
-        self.parent.after(20, self.engine)
-        
-def main():
-        
-    root = Tk()
-    GUI = Graphics(root, rockets_numb)
-    GUI.bind("<Button-1>", GUI.engine())
-    root.mainloop()
-
-
-if __name__ == '__main__':
-    main()"""
