4ebe588ec03010d288bd35e57e0339380916c6bb
Pavel <yanpavevg@yandex.ru>
2020-02-15 22:51:37 +0700

    Correct Euler for 4 equations system

--- Task 12 Euler Runge Kutte method/main.py
+++ Task 12 Euler Runge Kutte method/main.py
@@ -42,25 +42,24 @@ fig.show()'''
 
 
 def func1(x, y):
-    print("in func1")
-    return 2*(x*x + y)
+    return -2*y[0]+4*y[1]
 
 def func2(x, y):
-    return 2*(x*x + y)
+    return -y[0]+3*y[1]
 
 def func3(x, y):
-    return 2*(x*x + y)
+    return -2*y[0]+4*y[1]
 
 def func4(x, y):
-    return 2*(x*x + y)
+    return -y[0]+3*y[1]
 
 
-def calc_next(y, t, dt, func_arr):
+def calc_next(y, x, dx, func_arr):
     len_y = len(y)
     len_f_arr = len(func_arr)
     if len_y == len_f_arr:
         for i in range (0, len_y):
-            y[i] = y[i]+dt*func_arr[i](t,y[i])
+            y[i] = y[i]+dx*func_arr[i](x,y)
     
     return y
 
@@ -69,25 +68,25 @@ func_arr = [func1, func2, func3, func4]
 
 x_from = 0
 x_to = 1
-dx = 0.1
+N = 100
+dx = (x_to - x_from) / N
 
-y1_start = 1
-y2_start = 1
-y3_start = 1
-y4_start = 1
+y1_start = 3
+y2_start = 0
+y3_start = 3
+y4_start = 0
 
 #Initial precondition for the equations system
 y_start = [y1_start, y2_start, y3_start, y4_start]
 x = x_from
 
-
 y = y_start
-#res_y = [y1_start, y2_start, y3_start, y4_start]
 res_x = [x_from]
 res_y = [[y1_start],[y2_start],[y3_start],[y4_start]]
 
 
 while x<x_to:
+    print(y)
     y = calc_next(y, x, dx, func_arr)
 
     for i in range (0, len(y)):
@@ -96,14 +95,7 @@ while x<x_to:
     x+=dx
     res_x.append(x)
     
-
-
-
-def theory(x):
-    return 1.5*np.exp(2*x)-x*x-x-0.5
-
-
-x_theory = np.linspace(x_from, x_to, 10)
+x_theory = np.linspace(x_from, x_to, N)
 y_theory_1 = 4*np.exp(-x_theory) - np.exp(2*x_theory)
 y_theory_2 = np.exp(-x_theory)-np.exp(2*x_theory)
 
@@ -115,8 +107,8 @@ subplot.plot(x_theory, y_theory_1, 'b--', label='y1(x)theory')
 subplot.plot(x_theory, y_theory_2, 'r--', label='y2(x)theory')
 subplot.plot(res_x, res_y[0], 'b', label = 'y1(x) Euler')
 subplot.plot(res_x, res_y[1], 'r', label = 'y2(x) Euler')
-subplot.plot(res_x, res_y[2], 'black', label = 'y3(x) Euler')
-subplot.plot(res_x, res_y[3], 'yellow', label = 'y4(x) Euler')
+#subplot.plot(res_x, res_y[2], 'black', label = 'y3(x) Euler')
+#subplot.plot(res_x, res_y[3], 'yellow', label = 'y4(x) Euler')
 subplot.legend()
 fig.show()
     
