3b2bd389023d97fe763a41dd622c7e11c58d7862
Pavel <yanpavevg@yandex.ru>
2020-02-29 22:03:15 +0700

    Added sliding window shift

--- Sliding Window Filter/main.py
+++ Sliding Window Filter/main.py
@@ -113,7 +113,29 @@ class Function :
         res.y = y_
         return res
 
+    def plot_FFT(self):
+         # Частотная характеристика окна (Фильтра)
+        fft_lib_res = np.fft.fft(self.y)
+        spacing_period = (self.From-self.To)/self.N
 
+        xf = fftfreq(self.N, spacing_period)
+        lib_res_module = calc_module(fft_lib_res)
+
+        fig = plt.figure()
+        subplot = fig.add_subplot(111)
+        subplot.set_xlim(0, max(xf))
+        subplot.plot(fftshift(xf), abs(fftshift(lib_res_module))/self.N, label='Impulse characteristics')
+        subplot.legend()
+        fig.show()
+
+    def FFT(self):
+        fft_lib_res = np.fft.fft(self.y)
+        spacing_period = (self.From-self.To)/self.N
+
+        xf = fftfreq(self.N, spacing_period)
+        return [fftshift(xf), fftshift(fft_lib_res)]
+        
+        
 def calc_module(c_res):
     res = []
     for i in c_res:
@@ -121,7 +143,7 @@ def calc_module(c_res):
         res.append(module)
     return res
 
-def calc_and_plot(Filter_Func, Sample_Func):
+def calc_and_plot(Filter_Func, Sample_Func, title):
     # Результат фильтра (Свёртка)
     conv = np.convolve(Filter_Func.y, Sample_Func.y)
     x = np.linspace(Sample_Func.From, Sample_Func.To, len(conv))
@@ -136,6 +158,7 @@ def calc_and_plot(Filter_Func, Sample_Func):
 
 
     fig = plt.figure()
+    fig.suptitle(title)
 
     # Результат сглаживания 
     subplot = fig.add_subplot(131)
@@ -150,42 +173,107 @@ def calc_and_plot(Filter_Func, Sample_Func):
 
     # Фурье
     subplot = fig.add_subplot(133)
-    subplot.plot(fftshift(xf), abs(fftshift(lib_res_module))/Filter_Func.N, label='Filter Fourier image')
+    #subplot.set_xlim(0, max(xf))
+    subplot.plot(fftshift(xf), abs(fftshift(lib_res_module))/Filter_Func.N, label='Impulse characteristics')
     subplot.legend()
     
     fig.show()
 
-# Скользящее окно
+'''# Скользящее окно
 Window = Function(FuncParams(0, 3, 1, 1, 0, 100))
 Window.calc(step, 1, 2)
-#Window.plot()
+
 
 # Шумная ступенька
 meandr = Function((FuncParams(from_=0, to=3, ampl=1, omega=1, shift=0, N=500)))
 meandr.calc(meander, 1, 2)
 noize_level = 0.3
 meandr.noize(0.2, noize_level)
-#meandr.plot()
 
-# Результат фильтра
-conv = np.convolve(Window.y, meandr.y)
-x = np.linspace(0, 3, len(conv))
-#plt.plot(x, conv, color='r')
+calc_and_plot(Window, meandr, 'noizy step')
 
-# Частотная характеристика окна (Фильтра)
-fft_lib_res = np.fft.fft(Window.y)
-spacing_period = (Window.From-Window.To)/Window.N
 
-xf = fftfreq(Window.N, spacing_period)
-lib_res_module = calc_module(fft_lib_res)
+# Noizy sin
+Omega = 2
+noize_level=0.2
+noize_intensity=0.15
+sin_Func_a = Function((FuncParams(-np.pi*2, np.pi*2, 1, Omega, 0, 500)))
+sin_Func_a.calc(np.sin)
+sin_Func_a.noize(noize_intensity, noize_level)
 
-calc_and_plot(Window, meandr)
+calc_and_plot(Window, sin_Func_a, 'noizy sin, w='+str(Omega))'''
 
-#fig = plt.figure()
-#subplot = fig.add_subplot(111)
-#subplot.set_title('Filter Fourier')
-#subplot.plot(fftshift(xf), abs(fftshift(lib_res_module))/Window.N)
-#fig.show()
+
+def cut_filter(x_fft, y_fft, cut_percent):
+    res_y = []
+    res_x = []
+
+    middle_idx = int(len(x_fft)/2)
+    max_index = int(len(y_fft)*cut_percent / 2)
+    
+    #print(type(x_fft), type(y_fft))
+    #print('lens=', len(x_fft), len(y_fft))
+    res_y = y_fft.tolist()[middle_idx - max_index : middle_idx+max_index]
+    res_x = x_fft.tolist()[middle_idx - max_index : middle_idx + max_index]
+    return [res_x, res_y]
+    
+    
+    
+
+# Проверка на частотную характеристику ограниченного фильтра
+Filter_len_cut = 0.5 # процент, насколько усекаем множество значений импульсной характеристики (выбор длины фильтра)
+Freq_character = Function((FuncParams(from_=0, to=400, ampl=1, omega=1, shift=0, N=1000)))
+Freq_character.calc(meander, 0, 150)
+#Freq_character.plot()
+
+
+[x_fft, y_fft] = Freq_character.FFT()
+
+fig = plt.figure()
+subplot = fig.add_subplot(121)
+subplot.plot(x_fft, calc_module(y_fft), label='Impulse characteristics')
+
+
+[cut_fft_x, cut_fft_y] = cut_filter(x_fft, y_fft, Filter_len_cut) # срезаем часть фурье образа (выбор длины фильтра)
+subplot.plot(cut_fft_x, calc_module(cut_fft_y), label='Cut Impulse characteristics', color = 'r', marker='.')
+
+subplot.legend()
+
+
+# После обратного Фурье от урезанной характеристики фильтра
+res_y = np.fft.ifft(cut_fft_y)
+#res_y = calc_module(res_y)
+
+x = np.linspace(Freq_character.From, Freq_character.To, len(res_y))
+
+
+subplot = fig.add_subplot(122)
+subplot.plot(x, calc_module(res_y), color = 'green', marker = '.', label = 'Cut Filter Charact')
+subplot.plot(Freq_character.x, Freq_character.y, 'r--', label='Source Filter Charact')
+
+subplot.legend()
+fig.show()
+
+
+# Последний шаг - свертка сигнала c импульсной характеристикой
+Omega = 3
+sin_Func = Function((FuncParams(-np.pi*2, np.pi*2, 1, Omega, 0, 1000)))
+sin_Func.calc(np.sin)
+y = np.convolve(sin_Func.y, res_y)
+y = calc_module(y)
+x = np.linspace(-np.pi*2,np.pi*2, len(y))
+
+fig = plt.figure()
+subplot = fig.add_subplot(111)
+
+subplot.plot(sin_Func.x, sin_Func.y, label='Source Signal')
+subplot.plot(x, y, label='Filtered Signal', marker='.')
+subplot.legend()
+fig.show()
+
+
+
+
 
 
 
