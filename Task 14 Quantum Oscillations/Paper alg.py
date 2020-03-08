d6dcc26c9cd0d3b08ce96d73640365f8db0402ed
Pavel Ianko <yanpavevg@yandex.ru>
2020-03-08 17:48:55 +0700

    Сшивка +- работает

--- Task 14 Quantum Oscillations/Paper alg.py
+++ Task 14 Quantum Oscillations/Paper alg.py
@@ -76,7 +76,7 @@ def epsilon(eps, deps, N):
         if P1*P2 <0:
             deps = -deps/2
         eps = eps + deps
-        print('eps=' + str(eps) + 'deps=' + str(deps))
+        #print('eps=' + str(eps) + 'deps=' + str(deps))
         P1 = P2
     return eps
 
@@ -158,41 +158,54 @@ subplot = fig.add_subplot(111)
 subplot.plot(x, Psi)
 fig.show()'''
 
+def calc_Psi(eps_0, N, g2, FROM, TO, XIN, XOUT):
+    
+    
+    X_m = (XIN + XOUT) / 2
 
-
+    #TO = X_m     
+    x = np.linspace(FROM, TO, N)
+    k2 = calc_k2(eps_0, x)
+    
+   
+    dx = (FROM - TO) / (N-1)
+    dx2 =  dx ** 2
+    Psi = my_wavefunction(eps_0, k2, N, dx2)
+    return [Psi, x, X_m]
+    
+   
 # Експоненциально нарастающее решение слева
 eps_0 = -0.6
 [XIN, XOUT] = calc_XIN_XOUT(eps_0)
+
 X_m = (XIN + XOUT) / 2
-
-N = 100
-#Psi = np.zeros(N)
-g2 = 200 # gamma^2
-x = np.linspace(0.01, X_m, N)
-k2 = calc_k2(eps_0, x)
-print('k2=', k2)
-
-dx2 = ((1.0) / (N-1))**2 # dx^2
-Psi_left = my_wavefunction(eps_0, k2, N, dx2)
+[Psi_left, x_left, X_m] = calc_Psi(eps_0, 100, 200, 0.01, X_m, XIN, XOUT)
+x_continue = x_left[len(x_left) - 2] # значение в которой надо сосчитать
+print('x_continue'+str(x_continue))
 Psi_left = [-p for p in Psi_left]
+
+[Psi_right, x_right, X_m] = calc_Psi(eps_0, 100, 200, 3, x_continue, XIN, XOUT)
+
 fig = plt.figure()
-subplot = fig.add_subplot(111)
-subplot.plot(x, Psi_left)
+subplot = fig.add_subplot(121)
+subplot.plot(x_left, Psi_left, label = 'Psi left')
+subplot.legend()
+subplot = fig.add_subplot(122)
+subplot.plot(x_right, Psi_right, label = 'Psi right')
+subplot.legend()
 fig.show()
 
 
 # Експоненциально нарастающее решение справа (Интегрирование вовнутрь класс. разреш. области)
-x = np.linspace(3, X_m, N)
-k2 = calc_k2(eps_0, x)
+# - колво точек - N, но строим до X_m - h
 
-print('k2 right=', k2)
 
-Psi_right = my_wavefunction(eps_0, k2, N, dx2)
 
-fig = plt.figure()
+'''fig = plt.figure()
 subplot = fig.add_subplot(121)
-subplot.plot(x, Psi_right)
-#fig.show()
+subplot.plot(x_right, Psi_right, label = 'Psi right')
+subplot.legend()
+#fig.show()'''
 
 
 # нормировка, чтобы функции сшились в X_m
@@ -200,12 +213,96 @@ right_val = Psi_right[len(Psi_right)-1]
 left_val = Psi_left[len(Psi_left)-1]
 ratio = right_val / left_val
 Psi_left = [p*ratio for p in Psi_left]
-x = np.linspace(0.01, X_m, N)
+x = np.linspace(0.01, X_m, len(Psi_left))
 subplot = fig.add_subplot(122)
-subplot.plot(x, Psi_left)
+subplot.plot(x, Psi_left, label = 'Psi left')
+subplot.legend()
 fig.show()
 
+def my_epsilon_(eps, deps, N):
 
+    [XIN, XOUT] = calc_XIN_XOUT(eps)
+
+    X_m = (XIN + XOUT) / 2
+
+
+    [Psi_left, x_left, X_m] = calc_Psi(eps, N, 200, 0.01, X_m, XIN, XOUT)
+    x_continue = x_left[len(x_left) - 2] # значение в которой надо сосчитать
+       
+    Psi_left = [-p for p in Psi_left]
+    [Psi_right, x_right, X_m] = calc_Psi(eps, N, 200, 3, x_continue, XIN, XOUT)
+
+    # Нормировка
+    right_val = Psi_right[len(Psi_right)-1]
+    left_val = Psi_left[len(Psi_left)-1]
+    ratio = right_val / left_val
+    Psi_left = [p*ratio for p in Psi_left]
+
+
+    val_to_check_right = Psi_right[len(Psi_right) - 1]
+    val_to_check_left = Psi_left[len(Psi_left) - 2]
+
+    diff1 = val_to_check_right - val_to_check_left
+    print('diff1', diff1)
+    
+    while(abs(deps) > 1e-12):
+        [XIN, XOUT] = calc_XIN_XOUT(eps)
+
+        X_m = (XIN + XOUT) / 2
+        
+        [Psi_left, x_left, X_m] = calc_Psi(eps, N, 200, 0.01, X_m, XIN, XOUT)
+        x_continue = x_left[len(x_left) - 2] # значение в которой надо сосчитать
+        
+        Psi_left = [-p for p in Psi_left]
+
+        [Psi_right, x_right, X_m] = calc_Psi(eps, N, 200, 3, x_continue, XIN, XOUT)
+
+        # Нормировка
+        right_val = Psi_right[len(Psi_right)-1]
+        left_val = Psi_left[len(Psi_left)-1]
+        ratio = right_val / left_val
+        Psi_left = [p*ratio for p in Psi_left]
+
+
+        val_to_check_right = Psi_right[len(Psi_right) - 1]
+        val_to_check_left = Psi_left[len(Psi_left) - 2]
+       # print('vals to check:', val_to_check_right, val_to_check_left)
+
+        diff2 = val_to_check_right - val_to_check_left
+
+
+        if diff1*diff2 <0:
+            #print('shit')
+            deps = -deps/2
+        eps = eps + deps
+        #print('eps=' + str(eps) + 'deps=' + str(deps))
+        diff1 = diff2
+    return [eps, Psi_left, x_left, Psi_right, x_right]
+
+
+
+# точное значение энергии, через сшивку производных
+def test(eps):
+    [eps_real, Psi_left, x_left, Psi_right, x_right] = my_epsilon_(eps, 0.05, 100)
+    print('eps=', eps, ' eps actual=\n', eps_real)
+    fig = plt.figure()
+    subplot = fig.add_subplot(121)
+    subplot.plot(x_left, Psi_left, label = 'Psi left')
+    
+    subplot.plot(x_right, Psi_right, label = 'Psi right, eps='+str(eps))
+    subplot.legend()
+    fig.show()
+    
+#eps = my_epsilon_(eps_0, 0.05, 100)
+
+#test(-0.3)
+#test(-0.4)
+#test(-0.5)
+test(-0.6)
+test(-0.7)
+test(-0.75)
+test(-0.8)
+test(-0.9)
 
 
 
